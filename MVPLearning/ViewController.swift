6cb0a551f3e9dd7fa4dd0f1f0d78b51965129901
Atchibabu.Chitri <Atchibabu.Chitri@externals.hilti.com>
2021-02-05 07:30:05 +0530

    Initial commit for MVP Architecture

---
+++ MVPLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPLearning
+//
+//  Created by Chithri Atchibabu (BLR GSS) on 05/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
